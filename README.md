34caeb1f36b7442c11aaef9ee274be1cdb9cf09a
Guillaume "Vermeille" Sanchez <guillaume.v.sanchez@gmail.com>
2017-05-30 11:59:47 +0200

    add clang-callgraph.py

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # clang-callgraph
-A tool which generate a call graph from a given C++ based on clang
+A tool which generate a call graph from a given C++ based on clang. It excludes
+calls to the standard library so that only what matters to your app is shown.
+
+# Usage
+
+`./clang-callgraph.py file.cpp|compile_commands.json [extra clang args...]`
+
+The easiest way to generate the file compile\_commands.json for any make based
+compilation chain is to use Bear and recompile with `bear make`.
+
+When running the python script, after parsing all the codebase, you are
+prompted to type in the function's name for which you wan to obtain the
+callgraph
+
+# Example
+
+```
+$ bear make
+<output omitted>
+$ python2 ~/oauth_krb5/clang-analyze.py compile_commands.json -I/usr/lib/llvm-3.8/lib/clang/3.8.0/include/
+reading source files...
+/home/vermeille/CPAsim/src/module.cpp
+/home/vermeille/CPAsim/src/module/modulevalues.cpp
+/home/vermeille/CPAsim/src/main.cpp
+/home/vermeille/CPAsim/src/parser.cpp
+> main
+matching:
+main(int, char **)
+> main(int, char **)
+main(int, char **)
+  Parser::ParseModuleDef(std::istream &)
+    Parser::EatWord(std::istream &, const std::string &)
+      Parser::FuckSpaces(std::istream &)
+      Parser::EatChar(std::istream &, char)
+    Parser::ParseWord(std::istream &)
+      Parser::FuckSpaces(std::istream &)
+    Module::Module(const std::string &)
+    Parser::FuckSpaces(std::istream &)
+    Parser::EatChar(std::istream &, char)
+    Parser::FuckSpaces(std::istream &)
+    Parser::EatChar(std::istream &, char)
+    Module::AddInput(std::unique_ptr<WireDecl>)
+      WireDecl::name()
+      WireDecl::name()
+      WireDecl::name()
+    Parser::ParseWireDecl(std::istream &)
+      Parser::ParseWord(std::istream &)
+      Parser::FuckSpaces(std::istream &)
+      Parser::EatChar(std::istream &, char)
+      Parser::FuckSpaces(std::istream &)
+      Parser::ParseDecimalInt(std::istream &)
+        Parser::FuckSpaces(std::istream &)
+      Parser::FuckSpaces(std::istream &)
+      Parser::EatChar(std::istream &, char)
+      Parser::FuckSpaces(std::istream &)
+      WireDecl::WireDecl(const std::string &, int)
+```
+
---
+++ clang-callgraph.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python2
+
+from pprint import pprint
+from clang.cindex import CursorKind, Index, CompilationDatabase
+from collections import defaultdict
+import sys
+import json
+
+
+"""
+Dumps a callgraph of a function in a codebase
+usage: callgraph.py file.cpp|compile_commands.json [extra clang args...]
+The easiest way to generate the file compile_commands.json for any make based
+compilation chain is to use Bear and recompile with `bear make`.
+
+When running the python script, after parsing all the codebase, you are
+prompted to type in the function's name for which you wan to obtain the
+callgraph
+"""
+
+CALLGRAPH = defaultdict(list)
+FULLNAMES = defaultdict(list)
+
+def get_diag_info(diag):
+    return { 'severity' : diag.severity,
+             'location' : diag.location,
+             'spelling' : diag.spelling,
+             'ranges' : diag.ranges,
+             'fixits' : diag.fixits }
+
+def fully_qualified(c):
+    if c is None:
+        return ''
+    elif c.kind == CursorKind.TRANSLATION_UNIT:
+        return ''
+    else:
+        res = fully_qualified(c.semantic_parent)
+        if res != '':
+            return res + '::' + c.spelling
+        return c.spelling
+
+def fully_qualified_pretty(c):
+    if c is None:
+        return ''
+    elif c.kind == CursorKind.TRANSLATION_UNIT:
+        return ''
+    else:
+        res = fully_qualified(c.semantic_parent)
+        if res != '':
+            return res + '::' + c.displayname
+        return c.displayname
+
+def show_info(node, cur_fun=None):
+    if node.kind == CursorKind.FUNCTION_TEMPLATE:
+        if not node.extent.start.file.name.startswith('/usr'):
+            cur_fun = node
+            FULLNAMES[fully_qualified(cur_fun)].append(
+                    fully_qualified_pretty(cur_fun))
+
+    if node.kind == CursorKind.CXX_METHOD or \
+            node.kind == CursorKind.FUNCTION_DECL:
+        if not node.extent.start.file.name.startswith('/usr'):
+            cur_fun = node
+            FULLNAMES[fully_qualified(cur_fun)].append(
+                    fully_qualified_pretty(cur_fun))
+
+    if node.kind == CursorKind.CALL_EXPR:
+        if node.referenced and \
+            node.referenced.extent.start.file and \
+            not node.referenced.extent.start.file.name.startswith('/usr'):
+                CALLGRAPH[fully_qualified_pretty(cur_fun)].append(node.referenced)
+
+    for c in node.get_children():
+        show_info(c, cur_fun)
+
+def print_calls(fun_name, so_far, depth=0):
+    if depth >= 15:
+        print('...<too deep>...')
+        return
+    if fun_name in CALLGRAPH:
+        for f in CALLGRAPH[fun_name]:
+            print('  ' * (depth + 1) + fully_qualified_pretty(f))
+            if f in so_far:
+                continue
+            so_far.append(f)
+            if fully_qualified_pretty(f) in CALLGRAPH:
+                print_calls(fully_qualified_pretty(f), so_far, depth + 1)
+            else:
+                print_calls(fully_qualified(f), so_far, depth + 1)
+
+def read_compile_commands(filename):
+    if filename.endswith('.json'):
+        with open(filename) as compdb:
+            return json.load(compdb)
+    else:
+        return [{'command': '', 'file': filename}]
+
+def main():
+    if len(sys.argv) < 2:
+        print('usage: ' + sys.argv[0] + ' file.cpp|compile_database.json '
+                '[extra clang args...]')
+        return
+
+    print('reading source files...')
+    for cmd in read_compile_commands(sys.argv[1]):
+        index = Index.create()
+        c = [x for x in cmd['command'].split()
+                if x.startswith('-I') or x.startswith('-std=') or
+                x.startswith('-D')] + sys.argv[2:]
+        tu = index.parse(cmd['file'], c)
+        print(cmd['file'])
+        if not tu:
+            parser.error("unable to load input")
+
+        for d in tu.diagnostics:
+            if d.severity == d.Error or d.severity == d.Fatal:
+                print(' '.join(c))
+                pprint(('diags', map(get_diag_info, tu.diagnostics)))
+                return
+        show_info(tu.cursor)
+
+    while True:
+        fun = raw_input('> ')
+        if not fun:
+            break
+        if fun in CALLGRAPH:
+            print(fun)
+            print_calls(fun, [])
+        else:
+            print('matching:')
+            for f, ff in FULLNAMES.iteritems():
+                if f.startswith(fun):
+                    for fff in ff:
+                        print(fff)
+
+if __name__ == '__main__':
+    main()
+
